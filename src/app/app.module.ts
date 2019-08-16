d550642e4b3730b50f0f8c95533529b9f329c10b
Jayappa <akshay.jayappa@epsilon.com>
2019-08-16 12:22:13 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartjsComponent } from './chartjs/chartjs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartjsComponent
   ],
   imports: [
     BrowserModule,
